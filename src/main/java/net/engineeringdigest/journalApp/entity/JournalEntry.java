e04a1d4c6b1bf08e514f74953ee795e4668edfd1
Abhilash107 <abhilash00003108884@gmail.com>
2025-07-09 01:16:03 +0530

    Add Lombok dependency and refactor JournalEntry class to use Lombok annotations

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,5 +1,9 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +12,12 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+//@Getter
+//@Setter
+//@ToString
+
+// or // use Lombok's @Data annotation which combines @Getter, @Setter, @ToString, @EqualsAndHashCode, and @RequiredArgsConstructor
+@Data
 public class JournalEntry {
 
     @Id//primary key
@@ -17,36 +27,37 @@ public class JournalEntry {
 
     private Date date;
 
-    public Date getDate() {
-        return date;
-    }
+    //no need for a constructor as Lombok's @Getter and @Setter will handle it
+//    public Date getDate() {
+//        return date;
+//    }
+//
+//    public String getContent() {
+//        return content;
+//    }
+//
+//    public void setContent(String content) {
+//        this.content = content;
+//    }
+//
+//
+//    public String getTitle() {
+//        return title;
+//    }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
 
     public Object getId() {
         return id;
     }
 
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+//    public void setDate(Date date) {
+//        this.date = date;
+//    }
+//
+//    public void setId(ObjectId id) {
+//        this.id = id;
+//    }
 }
